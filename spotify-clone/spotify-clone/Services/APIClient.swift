43b18b156e48634eb62f2a91fe844c12bb4ee007
Daniel Belmonte <dani.belmonte.valero@gmail.com>
2023-06-10 14:37:26 +0200

    Add ApiClient

--- spotify-clone/spotify-clone/Services/APIClient.swift
+++ spotify-clone/spotify-clone/Services/APIClient.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 class APIClient {
-    
+    // MARK: - Fetch Data
+
+    func fetchData(from url: URL) async throws -> Data {
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NSError(domain: "InvalidResponse", code: 0, userInfo: nil)
+        }
+
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw NSError(domain: "HTTPError", code: httpResponse.statusCode, userInfo: nil)
+        }
+
+        /// Use JSONDecoder with correct data model before catch data
+        return data
+    }
 }
